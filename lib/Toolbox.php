915c71c92e63a0658799e62c10e94043671ba4f6
William Bittner <wjb5174@psu.edu>
2015-12-29 21:46:58 -0500

    Remove unused function from toolbox

--- lib/Toolbox.php
+++ lib/Toolbox.php
@@ -21,13 +21,5 @@
 	}
  }
  
- //query the database to see if the username is available
- //PRE: Username is sanitized!
- function isUsernameAvailable( $username, $conn )
- {
-	$result = $conn->query( 'SELECT * FROM player WHERE username='.$username );
-	
-	return mysqli_num_rows( $result ) == 0;
- }
  
 ?>
